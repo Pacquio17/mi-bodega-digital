fc4d6712c20b01cd29be37da2470663cdfd6bba1
Pacquio17 <144076342+Pacquio17@users.noreply.github.com>
2025-11-09 16:59:08 -0500

    app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 # Importar las herramientas de Flask
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
+import os
 
 # Crear la aplicación Flask
 app = Flask(__name__)
@@ -103,4 +104,5 @@ def editar_producto(producto_id):
 # INICIAR LA APLICACIÓN
 if __name__ == '__main__':
     init_db()
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
